508946b7e1f755e172f6c357252e2d764032ef19
Jose Luis SANCHEZ-LOPEZ <joselusl@hotmail.com>
2026-08-27 17:26:21 +0200

    minor updates for compatibility

--- setup.py
+++ setup.py
@@ -20,7 +20,6 @@ setup(
  maintainer_email='joseluis.sanlop@gmail.com',
  description='TODO: Package description',
  license='BSD',
- tests_require=['pytest'],
  entry_points={'console_scripts': [
  		'ars_sim_obstacles_detector_ros_node = ars_sim_obstacles_detector.ars_sim_obstacles_detector_ros_node:main',
         ],},
